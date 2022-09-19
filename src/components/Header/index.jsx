52e5873fedd38d186f432ba567a232796c4e8fe7
Caroline <contatocarolinefeijo@gmail.com>
2022-09-19 16:53:33 -0300

    creation of header and style components

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './style.css'
+import logo from '../../assets/img/logo.png';
+import login from '../../assets/img/login.png'
+
+export default function Header() {
+    return (
+        <div className='container'>
+
+            <div className='logo-container'>
+                <img className='logo' src={logo} alt='falha logo'></img>
+                <h2 className='title-logo-red'>Baita</h2>
+                <h2 className='title-logo-green'>chef</h2>
+            </div>
+
+            <div className='container-align '>
+                <a className='title-section'>Saúdavel</a>
+                <a className='title-section'>Salgados</a>
+                <a className='title-section'>Doces</a>
+                <a className='title-section'>Rápidos</a>
+
+                <div className='button-login'>
+                <img className='login-user' src={login} alt='falha logo'></img>
+
+                </div>
+            </div>
+
+
+
+        </div>
+    )
+}
